5bd76c00c65462271d4e7a68b3ce38fa474720c9
kikluz <savir81@hotmail.com>
2022-06-12 22:51:37 -0700

    add new code

--- config.rb
+++ config.rb
@@ -51,6 +51,12 @@ require 'sprockets/es6'
 activate :sprockets do |s|
   s.supported_output_extensions << '.es6'
 end
+::Sprockets::ES6.configure do |config|
+  config.marshal_load({
+    modules: 'amd',
+    moduleIds: true
+  })
+end
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
